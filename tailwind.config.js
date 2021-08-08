67832707352bf5b4cfb98719f44c27eec8c6834c
ManuelPuchner <75803474+ManuelPuchner@users.noreply.github.com>
2021-08-08 22:59:33 +0200

    todo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  // purge: [],
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: [],
+  /**
+   * @todo purge messes up some stuff with the gradient borders
+   */
+  // purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
