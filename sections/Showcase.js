663d05c6859347831807791d1b1fbcb00d85f938
ManuelPuchner <manuelpuchner@icloud.com>
2021-08-18 19:02:05 +0000

    some changes

--- sections/Showcase.js
+++ sections/Showcase.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 import styles from "styles/Showcase.module.css";
 function Showcase() {
+
+  const [message, setMessage ] = useState("");
   const test = async () => {
     const data = await fetch(
-      `${window.location.protocol}//${window.location.host}/api/test?testprop=${"dies ist ein test"}&test=moin`
+      `${window.location.protocol}//${window.location.host}/api/test?testprop=${message}`
     );
   };
   const test2 = async () => {
@@ -12,10 +16,12 @@ function Showcase() {
     <div
       className={`
         showcase ${styles.showcase}
+        p-4
       `}
     >
-      <button onClick={test}>add</button>
-      <button onClick={test2}>clear</button>
+      <input id="input" type="text" className="p-2 border-2 rounded-sm" placeholder="enter message" onChange={(e) => {setMessage(e.currentTarget.value)}}/>
+      <button onClick={test} className="p-2 border-2 rounded-sm ml-1 bg-green-600 text-white">add</button>
+      <button onClick={test2} className="p-2 border-2 rounded-sm ml-1 bg-red-600 text-white">clear</button>
     </div>
   );
 }
