aa037c1e3342a458e16d8a17bf61c519087235f7
ManuelPuchner <75803474+ManuelPuchner@users.noreply.github.com>
2021-08-18 20:22:31 +0200

    changes

--- components/Footer.js
+++ components/Footer.js
@@ -1,23 +1,7 @@
 function Footer() {
   return (
-    <div
-      className="
-        w-full
-        bg-gradient-to-r from-red-500 via-pink-500 to-blue-800
-        rounded-t-lg
-        p-3
-        hidden
-      "
-    >
-      Photos provided by{" "}
-      <a
-        href="https://unsplash.com"
-        target="_blank"
-        rel="noreferrer"
-        className="underline"
-      >
-        unsplash.com
-      </a>
+    <div>
+      
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>PartyLocate</title>
+
       </Head>
       <Layout isHomepage>
         <Showcase />
