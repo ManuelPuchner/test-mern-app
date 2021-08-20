efc28eb71285a8d571c38ab501e966dcb07d47f5
ManuelPuchner <75803474+ManuelPuchner@users.noreply.github.com>
2021-08-20 17:10:36 +0200

    cool new changes

--- pages/api/removebyid.js
+++ pages/api/removebyid.js
@@ -1,10 +1,11 @@
+import { ObjectId } from "mongodb";
 import { connectToDatabase } from "util/mongodb";
 
 export default async function handler(req, res) {
   const { db } = await connectToDatabase();
 
-  
-  const response = await db.collection("testcol2").deleteMany({})
+  const data = req.query;
+  const response = await db.collection("testcol2").deleteOne({"_id": ObjectId(data.id)})
 
   res.json(response);
 }
--- sections/Showcase.js
+++ sections/Showcase.js
@@ -1,23 +1,36 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 import styles from "styles/Showcase.module.css";
 function Showcase() {
-
-  const [message, setMessage ] = useState("");
-  const [listMessages, setListMessages ] = useState([]);
+  const [message, setMessage] = useState("");
+  const [listMessages, setListMessages] = useState([]);
   const test = async () => {
+    clearInput();
     const data = await fetch(
       `${window.location.protocol}//${window.location.host}/api/test?testprop=${message}`
     );
   };
-  const test2 = async () => {
-    const data = await fetch(`${window.location.protocol}//${window.location.host}/api/clear`);
-  };
   const updateList = async () => {
-    const response = await fetch(`${window.location.protocol}//${window.location.host}/api/hello`);
+    const response = await fetch(
+      `${window.location.protocol}//${window.location.host}/api/hello`
+    );
     const data = await response.json();
-    setListMessages(data)
-    console.log(data)
+    setListMessages(data);
+  };
+
+  const textInput = useRef();
+
+  const clearInput = () => (textInput.current.value = "");
+
+  // get list on load
+  useEffect(() => {
+    updateList();
+  }, []);
+
+  const removeItem = async (id) => {
+    const response = await fetch(
+      `${window.location.protocol}//${window.location.host}/api/removebyid?id=${id}`
+    );
   }
   return (
     <div
@@ -26,27 +39,56 @@ function Showcase() {
         p-4
       `}
     >
-      <input id="input" type="text" className="p-2 border-2 rounded-sm" placeholder="enter message" onChange={(e) => {setMessage(e.currentTarget.value)}}/>
-      <button onClick={test} className="p-2 border-2 rounded-sm ml-1 bg-green-600 text-white">add</button>
-      <button onClick={test2} className="p-2 border-2 rounded-sm ml-1 bg-red-600 text-white">clear</button>
+      <input
+        id="input"
+        type="text"
+        ref={textInput}
+        className="p-2 border-2 rounded-sm"
+        placeholder="enter message"
+        onChange={(e) => {
+          setMessage(e.currentTarget.value);
+        }}
+      />
+      <button
+        onClick={test}
+        className="p-2 border-2 rounded-sm ml-1 bg-green-600 text-white"
+      >
+        add
+      </button>
+      <button
+        onClick={clearInput}
+        className="p-2 border-2 rounded-sm ml-1 bg-red-600 text-white"
+      >
+        clear
+      </button>
 
-      <button className="w-full border-2 rounded-sm p-2 mt-4 bg-blue-400" onClick={updateList}>
-        see all messages/update messages list
+      <button
+        className="w-full border-2 rounded-sm p-2 mt-4 bg-blue-400"
+        onClick={updateList}
+      >
+        update messages list
       </button>
 
       <div className="list mt-6">
         <h1 className="font-bold text-xl mb-1">These are all the messages:</h1>
-        {
-          listMessages.map(
-            listMessageObject => {
-              return (
-                <div className="p-2 border-t-2 border-b-2">
-                  <p>{listMessageObject.testprop}</p>
-                </div>
-              )
-            }
-          )
-        }
+        {listMessages.map((listMessageObject) => {
+          return (
+            <div
+              key={listMessageObject._id}
+              className="p-2 border-t-2 border-b-2 mb-1 flex justify-between border-1 items-center"
+            >
+              <p>{listMessageObject.testprop}</p>
+              <button
+                className="py-1 px-2 border-red-400 border-2 rounded-sm"
+                onClick={() => {
+                  removeItem(listMessageObject._id);
+                }}
+              >
+                Remove
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
